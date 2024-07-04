1e280d6c14376d78d1960d4027575713ea2e2313
KarunakarAavulamanda <Aavulamanda.Karunakar@in.ey.com>
2024-07-04 11:47:51 +0530

    Button alignment changes code

--- lib/ui/home_screen/stepper_service_request_widget.dart
+++ lib/ui/home_screen/stepper_service_request_widget.dart
@@ -1,168 +1,127 @@
 import 'package:flutter/material.dart';
-
+ 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
-
+ 
   @override
   State<MainPage> createState() => _MainPageState();
 }
-
+ 
 class _MainPageState extends State<MainPage> {
   int _currentStep = 0;
 // Color(0xff053c6d)
   Widget controlsBuilder(context, details) {
-    return Column(
-      children: [
-        ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all(
-                Color(0xff053c6d),
-              ),
-              shape: WidgetStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(2),
-                ),
+    return Container(
+      width: 150,
+      alignment: Alignment.centerLeft,
+      child: ElevatedButton(
+          style: ButtonStyle(
+            // fixedSize: Size.fromWidth(320),
+            backgroundColor: WidgetStateProperty.all(
+              Color(0xff053c6d),
+            ),
+            shape: WidgetStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(2),
               ),
             ),
-            onPressed: details.onStepContinue,
-            child: Text(
-              _currentStep >= 4 ? 'Finish' : 'Proceed',
-              style: TextStyle(color: Colors.white),
-            )),
-        // OutlinedButton(onPressed: details.onStepCancel, child: Text('Go to Previous Step'))
-      ],
+          ),
+          onPressed: details.onStepContinue,
+          child: Text(
+            _currentStep >= 4 ? 'Finish' : 'Proceed',
+            style: TextStyle(color: Colors.white),
+          )),
     );
   }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(100),
-        child: Theme(
-          data: ThemeData(
-            colorScheme: Theme.of(context).colorScheme.copyWith(
-                  primary: Colors.orange,
+        child: Stepper(
+          elevation: 0,
+          steps: [
+            Step(
+                title: Text('Case Initiation'),
+                content: Text(''),
+                isActive: _currentStep >= 0,
+                state: _currentStep == 0 ? StepState.editing : _currentStep > 0 ? StepState.complete : StepState.disabled,
+                stepStyle: StepStyle(
+                  color: _currentStep == 0 ? Colors.orange : _currentStep > 0 ? Colors.green : Colors.grey,
+                  connectorColor: Colors.grey,
+                )
                 ),
-          ),
-          child: Stepper(
-            elevation: 0,
-            steps: [
-              Step(
-                  title: Text('Case Initiation'),
-                  content: Text(''),
-                  isActive: _currentStep >= 0,
-                  state: _currentStep == 0
-                      ? StepState.editing
-                      : _currentStep > 0
-                          ? StepState.complete
-                          : StepState.disabled,
-                  stepStyle: StepStyle(
-                    color: _currentStep == 0
-                        ? Colors.orange
-                        : _currentStep > 0
-                            ? Colors.green
-                            : Colors.grey,
-                    connectorColor: Colors.grey,
-                  )),
-              Step(
-                  //_currentStep > 1 ? StepState.complete : StepState.disabled,
-                  title: Text('Customer Acknowledgement'),
-                  content: Text(''),
-                  isActive: _currentStep >= 1,
-                  state: _currentStep == 1
-                      ? StepState.editing
-                      : _currentStep > 1
-                          ? StepState.complete
-                          : StepState.disabled,
-                  stepStyle: StepStyle(
-                    color: _currentStep == 1
-                        ? Colors.orange
-                        : _currentStep > 1
-                            ? Colors.green
-                            : Colors.grey,
-                    connectorColor: Colors.grey,
-                  )),
-              Step(
-                  title: Text('Upload Documents'),
-                  content: Text(''),
-                  isActive: _currentStep >= 2,
-                  state: _currentStep == 2
-                      ? StepState.editing
-                      : _currentStep > 2
-                          ? StepState.complete
-                          : StepState.disabled,
-                  stepStyle: StepStyle(
-                    color: _currentStep == 2
-                        ? Colors.orange
-                        : _currentStep > 2
-                            ? Colors.green
-                            : Colors.grey,
-                    connectorColor: Colors.grey,
-                  )),
-              Step(
-                  title: Text('Request Submission'),
-                  content: Text(''),
-                  isActive: _currentStep >= 3,
-                  state: _currentStep == 3
-                      ? StepState.editing
-                      : _currentStep > 3
-                          ? StepState.complete
-                          : StepState.disabled,
-                  stepStyle: StepStyle(
-                    color: _currentStep == 3
-                        ? Colors.orange
-                        : _currentStep > 3
-                            ? Colors.green
-                            : Colors.grey,
-                    connectorColor: Colors.grey,
-                  )),
-              Step(
-                  title: Text('Triggers'),
-                  content: Text(''),
-                  isActive: _currentStep >= 4,
-                  state: _currentStep == 4
-                      ? StepState.editing
-                      : _currentStep > 4
-                          ? StepState.complete
-                          : StepState.disabled,
-                  stepStyle: StepStyle(
-                    color: _currentStep == 4
-                        ? Colors.orange
-                        : _currentStep > 4
-                            ? Colors.green
-                            : Colors.grey,
-                    connectorColor: Colors.grey,
-                  )
-                  // stepStyle: StepStyle(
-                  //   color: Colors.orange,
-                  //   connectorColor: Colors.grey,
-                  // )
-                  ),
-            ],
-            onStepTapped: (int newIndex) {
+            Step(//_currentStep > 1 ? StepState.complete : StepState.disabled,
+                title: Text('Customer Acknowledgement'),
+                content: Text(''),
+                isActive: _currentStep >= 1,
+                state: _currentStep == 1 ? StepState.editing : _currentStep > 1 ? StepState.complete : StepState.disabled,
+                stepStyle: StepStyle(
+                  color: _currentStep == 1 ? Colors.orange : _currentStep > 1 ? Colors.green : Colors.grey,
+                  connectorColor: Colors.grey,
+                )
+               
+                ),
+            Step(
+                title: Text('Upload Documents'),
+                content: Text(''),
+                isActive: _currentStep >= 2,
+                state: _currentStep == 2 ? StepState.editing : _currentStep > 2 ? StepState.complete : StepState.disabled,
+                stepStyle: StepStyle(
+                  color: _currentStep == 2 ? Colors.orange : _currentStep > 2 ? Colors.green : Colors.grey,
+                  connectorColor: Colors.grey,
+                )
+               
+                ),
+            Step(
+                title: Text('Request Submission'),
+                content: Text(''),
+                isActive: _currentStep >= 3,
+                state: _currentStep == 3 ? StepState.editing : _currentStep > 3 ? StepState.complete : StepState.disabled,
+                stepStyle: StepStyle(
+                  color: _currentStep == 3 ? Colors.orange : _currentStep > 3 ? Colors.green : Colors.grey,
+                  connectorColor: Colors.grey,
+                )
+               
+                ),
+            Step(
+                title: Text('Triggers'),
+                content: Text(''),
+                isActive: _currentStep >= 4,
+                state: _currentStep == 4 ? StepState.editing : _currentStep > 4 ? StepState.complete : StepState.disabled,
+                stepStyle: StepStyle(
+                  color: _currentStep == 4 ? Colors.orange : _currentStep > 4 ? Colors.green : Colors.grey,
+                  connectorColor: Colors.grey,
+                )
+                // stepStyle: StepStyle(
+                //   color: Colors.orange,
+                //   connectorColor: Colors.grey,
+                // )
+                ),
+          ],
+          onStepTapped: (int newIndex) {
+            setState(() {
+              _currentStep = newIndex;
+            });
+          },
+          currentStep: _currentStep,
+          onStepContinue: () {
+            if (_currentStep != 5) {
               setState(() {
-                _currentStep = newIndex;
+                _currentStep += 1;
               });
-            },
-            currentStep: _currentStep,
-            onStepContinue: () {
-              if (_currentStep != 5) {
-                setState(() {
-                  _currentStep += 1;
-                });
-              }
-            },
-            onStepCancel: () {
-              if (_currentStep != 0) {
-                setState(() {
-                  _currentStep -= 1;
-                });
-              }
-            },
-            type: StepperType.horizontal,
-            controlsBuilder: controlsBuilder,
-          ),
+            }
+          },
+          onStepCancel: () {
+            if (_currentStep != 0) {
+              setState(() {
+                _currentStep -= 1;
+              });
+            }
+          },
+          type: StepperType.horizontal,
+          controlsBuilder: controlsBuilder,
         ),
       ),
     );
